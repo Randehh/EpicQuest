effcc5e84c9c026c0cedaa628a17741a637255b6
bigbeno37 <bigbeno37@gmail.com>
2015-01-11 21:34:11 +1100

    Seeing if tag is always applied to commits

--- src/test/java/CommandsTest.java
+++ src/test/java/CommandsTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CommandsTest {
-	//TODO: Add working tests
+	//TODO: Add working tests for this class
 	@Before
 	public void setUp() throws Exception {
 	}
